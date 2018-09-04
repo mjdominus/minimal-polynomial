3be6a3892a898abb801ba782950e6c81339b2def
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2018-09-04 12:59:05 -0400

    start new polynomial module, this time based on assoc lists

---
+++ Assoc.hs
@@ -0,0 +1,76 @@
+
+-- Assoc lists
+module Assoc where
+
+import Data.Maybe
+
+-- maps key to values
+data Assoc k v = Assoc [(k,v)] deriving Show
+empty = Assoc []
+isEmpty (Assoc []) = True
+isEmpty _ = False
+
+-- apply a function to just the key
+-- or to just the value
+key f = f . fst
+val f = f . snd
+
+-- apply a function to just the key
+-- or to just the value and return the resulting kvp
+keym f = \(k, v) -> (f k,   v)
+valm f = \(k, v) -> (  k, f v)
+
+kvm f g = \(k, v) -> (f k, g v)
+
+instance (Eq k, Eq v) => Eq (Assoc k v) where
+  Assoc [] == Assoc []   = True
+  Assoc _  == Assoc []   = False
+  Assoc as == Assoc ((bk,bv):bs) =
+    get_mb (Assoc as) bk == Just bv   &&
+    ((Assoc as) `remove` bk) == (Assoc bs)
+
+-- fmap maps over values, but not over keys
+instance Functor (Assoc k) where
+  fmap f (Assoc ls) = Assoc $ map (\(a,b) -> (a, f b)) ls
+
+-- folds up values, not keys
+instance Foldable (Assoc k) where
+  foldMap f (Assoc as) = foldMap (val f) as
+
+-- Assoc k v -> k -> Maybe v   (never fails)
+(Assoc []) `get_mb` k = Nothing
+(Assoc ((k',v):as)) `get_mb` k =
+    if k == k' then Just v else (Assoc as) `get_mb` k
+
+-- Assoc k v -> k -> v   (but might fail)
+as `get` k =
+  case as `get_mb` k of Nothing -> undefined
+                        Just v  -> v
+
+-- Assoc k v -> k -> Bool
+as `has` k = isJust $ as `get_mb` k
+
+-- Assoc k v -> k -> Assoc k v
+-- (Assoc []) `remove` k = empty
+remove (Assoc as) k = Assoc $ remove_ as k where
+  remove_ [] _ = []
+  remove_ ((k',v):as) k =
+    if k == k' then rest
+    else (k',v):rest
+         where rest = remove_ as k
+
+-- Assoc k v -> (k, v) -> Assoc k v
+as `upd` (k,v) = Assoc $ (k,v):rest where
+  Assoc rest = remove as k
+
+-- lift a one-argument function, such as filter,
+-- from lists to assocs
+lift f x = fmap (f x)
+-- the same, for two-argument functions
+lift2 f x1 x2 = fmap (f x1 x2)
+
+a1 = empty `upd` (1, "one")
+a2 = a1 `upd` (2, "two")
+a2' = empty `upd` (2, "two") `upd` (1, "one")
+a2'' = a2 `upd` (1, "another one")
+
--- Polynomial.hs
+++ Polynomial.hs
@@ -1,7 +1,29 @@
 
 module Polynomial where
+import Assoc
+import Data.List (sort)
+
 
 -- A polynomial over some type of coefficients (a)
 -- is just an assoc list of (power, coefficient) pairs
-data Poly a = Poly [(Integer, a)]
-zero = Poly []
+data Poly a = Poly (Assoc Integer a)
+zero = Poly empty
+assoc (Poly a) = a
+
+is_zero (Poly as) = all (key (== 0)) as
+
+instance Functor Poly where
+  fmap f (Poly a) = Poly $ fmap f a
+
+instance Eq a => Eq (Poly a) where
+  (Poly as) == (Poly bs)    = as == bs
+
+-- take list of coefficients in **decreasing* power order
+-- and build a polynomial
+-- for example, takes [1, -6, 11, 6] and produces x^3 - 6x^2 + 11x + 6,
+-- which is represented as 
+-- Poly (Assoc [(0,6),(1,11),(2,-6),(3,1)])
+poly coeffs = Poly (Assoc (filter ((/= 0) . snd) (index 0 (reverse coeffs)))) where
+  index _ [] = []
+  index n (a:as) = (n, a):(index (n+1) as)
+
