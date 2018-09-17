247b3e6b4ec7e2ee3ff2b6b09578ec3bd9b67722
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2018-09-16 20:35:18 -0400

    Module for "terms", which are like coefficient-less monomials
    A term is represented as a sequence of the powers on its variables in
    some canonical order.  So for example xy^2z^2 is represented as
    Term [1, 2, 2].

--- Term.hs
+++ Term.hs
@@ -2,22 +2,33 @@
 module Term where
 
 -- A coefficient-less term like [2, 3] means x^2 * y^3
-newtype Term = Term [Int]
+newtype Term = Term [Integer]
 
--- Just trim the trailing zeroes
-normalize (Term t) = Term $ reverse . (dropWhile (== 0)) . reverse $ t
+instance Eq Term where
+  s == t     = (pows . normalize) s == (pows . normalize) t where
+    -- Just trim the trailing zeroes
+    normalize (Term t) = Term $ reverse . (dropWhile (== 0)) . reverse $ t
 
 -- this is going to need some adjustments for special cases
 instance Show Term where
-  show (Term t) = show_ "xyztw" t of ""
+  show (Term []) = "T1"
+  show (Term t) = 'T' : show_ "xyztw" t where
     show_ (v:vs) []     = ""
     show_ (v:vs) (0:ps) = show_ vs ps
     show_ (v:vs) (1:ps) = [v] ++ show_ vs ps
     show_ (v:vs) (p:ps) = [v] ++ "^" ++ (show p) ++ show_ vs ps
-    
-    
-    
-    
-    
-  
 
+-- x^p
+mono p = Term [p]
+pows (Term ps) = ps
+
+degree (Term t) = sum t
+
+instance Ord Term where
+  compare s t =
+    case (degree s) `compare` (degree t) of
+      EQ -> (pows t) `compare` (pows s) -- reversed so x comes before y
+  -- reversed so that high degrees come before low degrees
+      GT -> LT
+      LT -> GT
+      
